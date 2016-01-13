1f4ec6b0e1c7d033591c30485ca9f3a5ade3206e
myaff <grabka74@gmail.com>
2016-01-13 11:38:37 +0500

    блок услуг с изменением
    вынесла в отдельный файл

---
+++ service-box.php
@@ -0,0 +1,49 @@
+<div class="service-box">
+	<div class="box-title">Наши услуги</div>
+	<div class="row valign-wrapper">
+		<div class="col s12 m6 l3 valign valign-wrapper">
+			<div class="card service valign">
+				<div class="card-icon center-align">
+					<a href="#"><img src="images/content/storage.png"/></a>
+				</div>
+				<div class="card-title center-align"><a href="#">Хранение товара</a></div>
+				<div class="card-text hide-on-small-only big-text">
+					<p>Хранение — этап обращения товара, который должен проходить в условиях, обеспечивающих минимальное изменение его количества и качества.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col s12 m6 l3 valign valign-wrapper">
+			<div class="card service valign">
+				<div class="card-icon center-align">
+					<a href="#"><img src="images/content/delivery.png"/></a>
+				</div>
+				<div class="card-title center-align"><a href="#">Доставка товаров</a></div>
+				<div class="card-text hide-on-small-only big-text">
+					<p>Мы предоставляем весь цикл услуг по доставке товаров: от забора со склада магазина до вручения конечному получателю. Мы выполняем доставку по России</p>
+				</div>
+			</div>
+		</div>
+		<div class="col s12 m6 l3 valign valign-wrapper">
+			<div class="card service valign">
+				<div class="card-icon center-align">
+					<a href="#"><img src="images/content/kredit.png"/></a>
+				</div>
+				<div class="card-title center-align"><a href="#">Рассрочка и кредитование</a></div>
+				<div class="cardm-text hide-on-small-only big-text">
+					<p>Последние годы банковский кредит приобрел необыкновенную популярность. Часто имеет место и продажа дорогостоящих товаров в рассрочку.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col s12 m6 l3 valign valign-wrapper">
+			<div class="card service valign">
+				<div class="card-icon center-align">
+					<a href="#"><img src="images/content/develop.png"/></a>
+				</div>
+				<div class="card-title center-align"><a href="#">Строительные и отделочные работы</a></div>
+				<div class="cardm-text hide-on-small-only big-text">
+					<p>При строительстве загородного дома и коттеджа, а так же  ремонте  здания важную роль отводят проведению наружных и внутренних отделочных работ.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- subcategory.php
+++ subcategory.php
@@ -94,54 +94,6 @@
 		</div>
 	</div>
 </div>
-<div class="service-box">
-	<div class="box-title">Наши услуги</div>
-	<div class="row valign-wrapper service-list">
-		<div class="col s12 m6 l3 valign valign-wrapper">
-			<div class="card service valign">
-				<div class="card-icon center-align">
-					<img src="images/content/storage.png"/>
-				</div>
-				<div class="card-title center-align">Хранение товара</div>
-				<div class="card-text hide-on-small-only big-text">
-					<p>Хранение — этап обращения товара, который должен проходить в условиях, обеспечивающих минимальное изменение его количества и качества.</p>
-				</div>
-			</div>
-		</div>
-		<div class="col s12 m6 l3 valign valign-wrapper">
-			<div class="card service valign">
-				<div class="card-icon center-align">
-					<img src="images/content/delivery.png"/>
-				</div>
-				<div class="card-title center-align">Доставка товаров</div>
-				<div class="card-text hide-on-small-only big-text">
-					<p>Мы предоставляем весь цикл услуг по доставке товаров: от забора со склада магазина до вручения конечному получателю. Мы выполняем доставку по России</p>
-				</div>
-			</div>
-		</div>
-		<div class="col s12 m6 l3 valign valign-wrapper">
-			<div class="card service valign">
-				<div class="card-icon center-align">
-					<img src="images/content/kredit.png"/>
-				</div>
-				<div class="card-title center-align">Рассрочка и кредитование</div>
-				<div class="cardm-text hide-on-small-only big-text">
-					<p>Последние годы банковский кредит приобрел необыкновенную популярность. Часто имеет место и продажа дорогостоящих товаров в рассрочку.</p>
-				</div>
-			</div>
-		</div>
-		<div class="col s12 m6 l3 valign valign-wrapper">
-			<div class="card service valign">
-				<div class="card-icon center-align">
-					<img src="images/content/develop.png"/>
-				</div>
-				<div class="card-title center-align">Строительные и отделочные работы</div>
-				<div class="cardm-text hide-on-small-only big-text">
-					<p>При строительстве загородного дома и коттеджа, а так же  ремонте  здания важную роль отводят проведению наружных и внутренних отделочных работ.</p>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+<?php include('service-box.php'); ?>
 <?php include('subscribe-box.php'); ?>
 <?php include('footer.php'); ?>
